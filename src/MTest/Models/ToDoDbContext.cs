797294bc34eed87c064028ff533018a25d7aa920
Jim Klein <j.mesklein@gmail.com>
2016-09-07 09:36:19 -0700

    Initial files. Initial migration complete.

---
+++ src/MTest/Models/ToDoDbContext.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+
+namespace ToDoListWithMigrations.Models
+{
+    public class ToDoDbContext : DbContext
+    {
+        public DbSet<Category> Categories { get; set; } 
+
+        public DbSet<Item> Items { get; set; }
+
+        public ToDoDbContext(DbContextOptions<ToDoDbContext> options)
+            : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
+    }
+}
