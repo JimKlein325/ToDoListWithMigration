65f1801997d2d70cb1142dad062ad876c28ef7b4
Jim Klein <j.mesklein@gmail.com>
2016-09-07 16:17:23 -0700

    add constructor injection of dbContext and IConfigurationRoot with connection string ref.

--- src/MTest/Models/ToDoDbContext.cs
+++ src/MTest/Models/ToDoDbContext.cs
@@ -4,31 +4,37 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection.Extensions;
+using Microsoft.Extensions.Configuration;
 
 namespace ToDoListWithMigrations.Models
 {
     public class ToDoDbContext : DbContext
     {
+        private IConfigurationRoot _config;
+
         public DbSet<Category> Categories { get; set; } 
 
         public DbSet<Item> Items { get; set; }
 
-        public ToDoDbContext(DbContextOptions<ToDoDbContext> options)
+        public ToDoDbContext(IConfigurationRoot config, DbContextOptions<ToDoDbContext> options)
             : base(options)
         {
+            _config = config;
+
         }
 
-        public ToDoDbContext()
-        {
-
-            //(@"Server=(localdb)\\MSSQLLocalDB;Database=ToDoListWithMigrations;Trusted_Connection=True;");
-        //var optionsBuilder = new DbContextOptionsBuilder<ToDoDbContext
-        //        >();
-        //    optionsBuilder.UseSqlServer(Configuration["Data:DefaultConnection:ConnectionString"]));
-        }
+        //public ToDoDbContext(IConfigurationRoot config)
+        //{
+        //    _config = config;
+        //}
         protected override void OnModelCreating(ModelBuilder builder)
         {
             base.OnModelCreating(builder);
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+            optionsBuilder.UseSqlServer(_config["Data:DefaultConnection:ConnectionString"]);
         }
     }
 }
